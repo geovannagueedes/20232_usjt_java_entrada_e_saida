8f3a8af8eae62fcf22f54919325b79d9bac6d477
Geovanna Guedes <geovanna.guedes2004@gmail.com>
2023-09-04 19:58:24 -0300

    ap02(input-outpu) resultado final v1

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.Scanner ;
+//pacote padrão : java.lang : importado automaticamente 
+public class SomaDoisNumeros {
+    public static void main(String[] args) {
+        //declaração de variável 
+        Scanner leitor = new Scanner(System.in); 
+        double primeiroOperando ;
+        double segundoOperando ;
+        double resultado ;
+    
+        //entrada
+        System.out.println("Digite o primeiro operando");
+        primeiroOperando = leitor.nextDouble() ;
+        System.out.println("Digite o segundo operando");
+        segundoOperando = leitor.nextDouble();
+        //processamento       
+        resultado = primeiroOperando+segundoOperando ;
+        //saída
+        System.out.print(resultado);
+
+    }
+}
